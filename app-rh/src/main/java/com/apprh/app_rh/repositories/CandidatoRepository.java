10e91b74ca8547dfe8ba9fe9c9e81c0789fcbef9
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2024-09-04 21:34:51 -0300

    refact: deleted comment

--- app-rh/src/main/java/com/apprh/app_rh/repositories/CandidatoRepository.java
+++ app-rh/src/main/java/com/apprh/app_rh/repositories/CandidatoRepository.java
@@ -10,5 +10,4 @@ public interface CandidatoRepository extends CrudRepository<Candidato, Long>  {
     Candidato findById(long id);
     Iterable<Candidato> findByVaga(Vaga vaga);    
     Candidato findByRg(String rg);
-    //List<Candidato> findByNomeCandidato(String nome);
 }
